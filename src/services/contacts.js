4bf6dde7ff023fb14cdedfc962e5fb8a769f3d71
denysskrypka <118266939+denysskrypka@users.noreply.github.com>
2024-09-23 15:17:09 +0300

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,3 +1,28 @@
 import { ContactsCollection } from '../db/models/Contacts.js';
 export const getAllContacts = () => ContactsCollection.find();
 export const getContactById = (id) => ContactsCollection.findById(id);
+export const createNewContacts = async (payload) => {
+  const contacts = await ContactsCollection.create(payload);
+  return contacts;
+};
+export const deleteContact = async (id) => {
+  const contact = await ContactsCollection.findOneAndDelete({
+    _id: id,
+  });
+  return contact;
+};
+export const patchContact = async (id, payload, options = {}) => {
+  const rawResult = await ContactsCollection.findOneAndUpdate(
+    {
+      _id: id,
+    },
+    payload,
+    { new: true, includeResultMetadata: true, ...options },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+  return {
+    contacts: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
+};
